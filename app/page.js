f476336013138c888d06fe4c87933dee299dcecf
ShuberAsh <96295801+ShuberAsh@users.noreply.github.com>
2023-10-27 02:04:04 +0800

    added login menu added some css styles

--- app/page.js
+++ app/page.js
@@ -5,13 +5,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-            <p>
-              testing;
-            </p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -31,17 +24,15 @@ export default function Home() {
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-  
+      <div className={styles.fill}>
+      
+        <input type="text" placeholder="Username"></input>
+        <input type="password" placeholder="Password"></input>
+        <input type="submit" value="Login"></input>
+        <div className={styles.line}></div>
+        <input type="submit" value="Sign Up"></input>
       </div>
+
       <div className={styles.grid}>
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
