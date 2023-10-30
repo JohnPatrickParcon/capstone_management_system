c9fb6635c9a8de210073443131b9f833b3699ad5
JohnPatrickParcon <johnpatrickparcon@gmail.com>
2023-10-30 12:53:27 +0800

    nextui table test

--- app/page.js
+++ app/page.js
@@ -2,12 +2,20 @@
 
 import { supabase } from "@/lib/initSupabase"
 import CapstoneCard from './ClientComponent/CapstoneCard'
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";
 
 export default function Home() {
   return (
     <>
       <main>
-        <CapstoneCard/>
+        <Table aria-label="Example empty table">
+          <TableHeader>
+            <TableColumn>NAME</TableColumn>
+            <TableColumn>ROLE</TableColumn>
+            <TableColumn>STATUS</TableColumn>
+          </TableHeader>
+          <TableBody emptyContent={"No rows to display."}>{[]}</TableBody>
+        </Table>
       </main>
     </>
   )
